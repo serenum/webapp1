0e7d53a1c231145d0b1df4a71681905c0edeb241
mac <9999>
2022-09-02 21:53:33 +0300

    open repo

---
+++ webapp/component.js
@@ -0,0 +1,22 @@
+sap.ui.define([
+    "sap/ui/core/UIComponent",
+    "sap/ui/model/json/JSONModel",
+    "sap/ui/model/resource/ResourceModel"
+], function (UIComponent, JSONModel) {
+    "use strict";
+    return UIComponent.extend("sap.ui.demo.walkthrough.Component", {
+        metadata: {
+            manifest: "json"
+        },
+        init: function () {
+            UIComponent.prototype.init.apply(this, arguments);
+            var oData = {
+                recipient: {
+                    name: "UI5"
+                }
+            };
+            var oModel = new JSONModel(oData);
+            this.setModel(oModel);
+        }
+    })
+})
